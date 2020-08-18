29b96054523de4fb8441e1c900f5e36a5114682a
Anatoly <anatolyuss@gmail.com>
2020-08-19 02:50:55 +0300

    improved Burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,13 +7,38 @@ class Burger extends Component {
     super(props);
   }
 
+  hasNoIngredients = () => {
+    return Object.keys(this.props.ingredients).length === 0
+      || Object.values(this.props.ingredients).reduce((acc, val) => {
+        acc += val;
+        return acc;
+      }, 0) === 0;
+  }
+
+  renderIngredients = () => {
+    const ingredients = { ...this.props.ingredients };
+
+    if (this.hasNoIngredients()) {
+      return <p>Please, add some ingredients.</p>;
+    }
+
+    return Object.keys(ingredients).map((ingredient, index) => {
+      const renderedIngredientOfSameType = [];
+
+      for (let i = 0; i < ingredients[ingredient]; ++i) {
+        renderedIngredientOfSameType.push(<BurgerIngredient type={ingredient} key={`${index}-${i}`} />);
+      }
+
+      return renderedIngredientOfSameType;
+    });
+  }
+
   render() {
     return (
-      <div className={ classes.Burger }>
-        <BurgerIngredient type="bread-top" />
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
-        <BurgerIngredient type="bread-bottom" />
+      <div className={classes.Burger}>
+        <BurgerIngredient type={'bread-top'} />
+        {this.renderIngredients()}
+        <BurgerIngredient type={'bread-bottom'} />
       </div>
     );
   }
--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -5,12 +5,20 @@ import Burger from '../Burger/Burger';
 class BurgerBuilder extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      ingredients: {
+        // salad: 3,
+        // meat: 0,
+        // cheese: 2,
+        // bacon: 0,
+      }
+    };
   }
 
   render() {
     return (
       <Auxiliary>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Auxiliary>
     );
